ccd616c26912686cbfeabeb317120d5f169eba67
panghyuk <okh9712@gmail.com>
2021-10-08 22:50:07 +0900

    day3

--- BOJ/class2/day3_1436.py
+++ BOJ/class2/day3_1436.py
@@ -1,2 +1,27 @@
 # 영화감독 숌
-# 브루트포스, 완전탐색 
+# 브루트포스, 완전탐색
+
+# sol.1
+n = int(input())
+war = 666
+
+while True:
+    if '666' in str(war):
+        n -= 1
+        if n == 0:
+            print(war)
+            break
+    war += 1
+
+# sol.2
+n = int(input())
+war = 666
+cnt = 0
+
+while True:
+    if '666' in str(war):
+        cnt += 1
+    if cnt == n:
+        print(war)
+        break
+    war += 1
