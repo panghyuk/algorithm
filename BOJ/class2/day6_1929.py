729c912d6232e5a93dd2710f6d4c29cd0b713f56
panghyuk <okh9712@gmail.com>
2021-10-31 20:35:27 +0900

    day6_2

--- BOJ/class2/day6_1929.py
+++ BOJ/class2/day6_1929.py
@@ -1,4 +1,32 @@
 # 소수 구하기
-# 수학
+# 수학, 에라토네스의 체
+import math
 
-print('yaho')
+def prime(x):
+    if x == 1:
+        return False
+    else:
+        for i in range(2,int(math.sqrt(x))+1): # 제곱근까지만 찾아보기
+            if x % i == 0:
+                return False
+        return True
+
+m,n = map(int,input().split())
+for j in range(m,n+1):
+    if prime(j):
+        print(j)
+
+# # 시간초과...
+# m,n = map(int,input().split())
+
+# def prime_over(x):
+#     if x < 2:
+#         return False
+#     for i in range(2,x):
+#         if x % i == 0:
+#             return False
+#     return True
+
+# for j in range(m,n+1):
+#     if prime_over(j):
+#         print(j)
