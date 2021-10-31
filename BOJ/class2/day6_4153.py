0c6a5affdf2a9419b1eb6e44d3f355c38fdb64c3
panghyuk <okh9712@gmail.com>
2021-10-31 20:44:10 +0900

    day6_3

---
+++ BOJ/class2/day6_4153.py
@@ -0,0 +1,12 @@
+# 직각삼각형
+# 수학, 기하학
+
+while True:
+    a,b,c = map(int,input().split())
+    if a == 0 and b == 0 and c == 0:
+        break
+    else:
+        if (a*a + b*b == c*c) or (a*a + c*c == b*b) or (b*b + c*c == a*a):
+            print('right')
+        else:
+            print('wrong')
