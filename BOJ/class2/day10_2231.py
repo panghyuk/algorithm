f5ae978f6ea0b2d10069f41428d951500f786724
panghyuk <okh9712@gmail.com>
2021-12-17 20:19:43 +0900

    day10_2

--- BOJ/class2/day10_2231.py
+++ BOJ/class2/day10_2231.py
@@ -2,5 +2,12 @@
 # 브루트포스 알고리즘
 
 n = int(input())
+for i in range(1,n+1):
+    gen = sum(map(int,str(i)))
+    result = i + gen
 
-
+    if result == n:
+        print(i)
+        break
+else:
+    print(0)
