145511e292ba45e14dfb680e2b5c26851d489100
panghyuk <okh9712@gmail.com>
2021-12-17 20:25:54 +0900

    day10_3

--- BOJ/class2/day10_2231.py
+++ BOJ/class2/day10_2231.py
@@ -2,12 +2,16 @@
 # 브루트포스 알고리즘
 
 n = int(input())
+
 for i in range(1,n+1):
     gen = sum(map(int,str(i)))
     result = i + gen
 
+    # result랑 n이랑 같으면 i 출력(가장 작은 값)
     if result == n:
         print(i)
         break
-else:
-    print(0)
+
+    # 생성자가 없을 경우 0 출력
+    if i == n:
+        print(0)
--- BOJ/class2/day11_2292.py
+++ BOJ/class2/day11_2292.py
@@ -1,1 +1,16 @@
 # 벌집
+n=int(input())
+f=1
+plus=6
+room=1
+if n==1:
+    print(1)
+else:
+    while True:
+        f += plus
+        room += 1
+        if n <= f:
+            print(room)
+            break
+        else:
+            plus += 6
