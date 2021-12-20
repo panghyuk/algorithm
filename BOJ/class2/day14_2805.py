d49753a7b7dd330faf3ba33faad734337c6835ae
panghyuk <okh9712@gmail.com>
2021-12-20 18:20:32 +0900

    day14

--- BOJ/class2/day14_2805.py
+++ BOJ/class2/day14_2805.py
@@ -1,2 +1,26 @@
 # 나무 자르기
 # 이분 탐색
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+tree = list(map(int,input().split()))
+
+start = 0
+end = max(tree)
+
+# 이분탐색 알고리즘
+while start <= end:
+    mid = (start+end)//2
+    cnt = 0 # 잘린 나무의 총합
+
+    for i in tree:
+        if mid < i:
+            cnt += i - mid    
+
+    if cnt < m:
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(end)
