8818251824fde3e92f56c3172b40823a226a3010
panghyuk <okh9712@gmail.com>
2021-12-22 16:03:50 +0900

    day17_2

--- BOJ/class2/day17_10773.py
+++ BOJ/class2/day17_10773.py
@@ -1,3 +1,17 @@
 # 제로
 # 스택(LIFO)
 
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+
+for i in range(n):
+    num = int(input())
+    if num != 0:
+        stack.append(num)
+    else:
+        stack.pop()
+
+print(sum(stack))
