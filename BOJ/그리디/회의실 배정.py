8096a5826d4d4ba2f85d22d6d22dfb271ff1a59a
panghyuk <okh9712@gmail.com>
2022-01-05 14:36:43 +0900

    1주차 스터디

--- BOJ/그리디/회의실 배정.py
+++ BOJ/그리디/회의실 배정.py
@@ -1,2 +1,20 @@
 # 1931
+import sys
 
+n = int(input())
+input = sys.stdin.readline
+
+meets = []
+for i in range(n):
+    meets.append(list(map(int,input().split())))
+
+meets.sort(key = lambda x:(x[1],x[0]))
+cnt = 0
+end = -1
+
+for meet in meets:
+    if end <= meet[0]:
+        end = meet[1]
+        cnt += 1
+
+print(cnt)
