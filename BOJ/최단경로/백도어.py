5ab04078e996842745af92e029185c5918db7a37
panghyuk <okh9712@gmail.com>
2022-02-11 15:23:02 +0900

    6주차 스터디

--- BOJ/최단경로/백도어.py
+++ BOJ/최단경로/백도어.py
@@ -2,31 +2,39 @@
 
 import heapq,sys
 input = sys.stdin.readline
-INF = int(1e9)
+INF = sys.maxsize
 
 n,m = map(int,input().split())
 vision = list(map(int,input().split()))
+vision[-1] = 0
 graph = [[] for _ in range(n)]
-dis = [INF] * (n+1)
+
 
 for _ in range(m):
-    a,b = map(int,input().split())
-    graph[a].append((b,1))
-    graph[b].append((a,1))
+    a,b,t = map(int,input().split())
+    graph[a].append((b,t))
+    graph[b].append((a,t))
 
-def dijkstra(start):
-    q = []
-    heapq.heappush(q,(0,start))
-    dis[start] = 0
 
-    while q:
-        d, now = heapq.heappop(q)
-        if d < dis[now]:
-            continue
+q = []
+heapq.heappush(q,(0,0))
+dis = [INF] * n
+dis[0] = 0
 
-        for i in graph[now]:
-            cost = d + i[1]
+while q:
+    d, now = heapq.heappop(q)
+    if d > dis[now]:
+        continue
 
-            if cost < dis[i[0]]:
+    for i in graph[now]:
+        cost = d + i[1]
+
+        if cost < dis[i[0]]:
+            if vision[i[0]] == 0:
                 dis[i[0]] = cost
                 heapq.heappush(q,(cost,i[0]))
+
+if dis[-1] == INF:
+    print(-1)
+else:
+    print(dis[-1])
