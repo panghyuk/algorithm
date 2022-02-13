b35e3496db8f90b57363f1436b38e6fd336dda69
panghyuk <okh9712@gmail.com>
2022-02-13 12:53:58 +0900

    7주차 스터디

--- 이코테/7주차_기타 그래프/사이클 판별.py
+++ 이코테/7주차_기타 그래프/사이클 판별.py
@@ -2,7 +2,7 @@
 def find_parent(parent,x):
     # 루트 노드를 찾을 때까지 재귀 호출
     if parent[x] != x:
-        return find_parent(parent,parent[x])
+        parent[x] = find_parent(parent,parent[x])
     return parent[x]
 
 # 두 원소가 속한 집합을 합치기
