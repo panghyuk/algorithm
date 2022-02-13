072552295876d38904957f12f2874b41a7524bc8
panghyuk <okh9712@gmail.com>
2022-02-13 12:21:25 +0900

    7주차 스터디

--- 이코테/7주차_기타 그래프/사이클 판별.py
+++ 이코테/7주차_기타 그래프/사이클 판별.py
@@ -3,7 +3,7 @@ def find_parent(parent,x):
     # 루트 노드를 찾을 때까지 재귀 호출
     if parent[x] != x:
         return find_parent(parent,parent[x])
-    return x
+    return parent[x]
 
 # 두 원소가 속한 집합을 합치기
 def union_parent(parent,a,b):
@@ -23,19 +23,18 @@ parent = [0] * (v+1) # 부모 테이블 초기화
 for i in range(1,v+1):
     parent[i] = i
 
-# Union 연산을 각각 수행
+cycle = False # 사이클 발생 여부
+
 for i in range(e):
     a,b = map(int,input().split())
-    union_parent(parent,a,b)
-
-# 각 원소가 속한 집합 출력
-print('각 원소가 속한 집합: ',end = " ")
-for i in range(1,v+1):
-    print(find_parent(parent,i), end = " ")
-
-print()
-
-# 부모 테이블 내용 출력하기
-print('부모 테이블: ',end = " ")
-for i in range(1,v+1):
-    print(parent[i],end = " ")
+    # 사이클이 발생한 경우 종료
+    if find_parent(parent,a) == find_parent(parent,b):
+        cycle = True
+        break
+    # 사이클이 발생하지 않았다면 합집합(Union) 연산 수행
+    else:
+        union_parent(parent,a,b)
+if cycle:
+    print("사이클이 발생했습니다.")
+else:
+    print("사이클이 발생하지 않았습니다.")
