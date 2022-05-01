fe4dfa9b9736834f80643dfb8a675655b4176501
panghyuk <okh9712@gmail.com>
2022-05-01 16:25:15 +0900

    day 34

--- BOJ/solved.ac/class3/day34_단지번호붙이기.py
+++ BOJ/solved.ac/class3/day34_단지번호붙이기.py
@@ -1,2 +1,38 @@
 # 2667
 
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+graph = []
+cnt = 0
+ans = []
+
+for i in range(n):
+    graph.append(list(map(int,input().strip())))
+
+
+def bfs(a,b):
+    q = deque([a,b])
+    
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = x + dy[i]
+
+            if nx <= -1 or nx >= n or ny <= -1 or ny >= n:
+                continue
+            if graph[nx][ny] == 1:
+                return
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            bfs(i,j)
+            cnt += 1
