a93dea7aa6b84a6b70aa2c1f8c1419a91d70a01c
panghyuk <okh9712@gmail.com>
2022-05-02 13:52:10 +0900

    day 35

--- BOJ/solved.ac/class3/day34_단지번호붙이기.py
+++ BOJ/solved.ac/class3/day34_단지번호붙이기.py
@@ -3,6 +3,16 @@
 from collections import deque
 import sys
 
+input = sys.stdin.readline
+n = int(input())
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+graph = []
+ans = []
+res = 0
+
 # bfs 실행
 def bfs(a,b):
     cnt = 1
@@ -22,17 +32,8 @@ def bfs(a,b):
                 graph[nx][ny] = 0
                 q.append([nx,ny])
                 cnt += 1
+
     ans.append(cnt)
-
-input = sys.stdin.readline
-n = int(input())
-
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
-graph = []
-ans = []
-res = 0
 
 # input 입력
 for _ in range(n):
---
+++ BOJ/solved.ac/class4/day35_N과M.py
@@ -0,0 +1,52 @@
+# # 15649 백트래킹
+
+# n,m = map(int,input().split())
+# ans = []
+
+# def dfs():
+#     if len(ans) == m:
+#         print(" ".join(map(str,ans)))
+#         return
+        
+#     for i in range(1,n+1):
+#         if i in ans:
+#             continue
+#         ans.append(i)
+#         dfs()
+#         ans.pop()
+
+# dfs()
+
+# # 15650 백트래킹(dfs)
+
+# n,m = map(int,input().split())
+# ans = []
+
+# def dfs(s):
+#     if len(ans) == m:
+#         print(" ".join(map(str,ans)))
+#         return
+
+#     for i in range(s,n+1):
+#         if i not in ans:
+#             ans.append(i)
+#             dfs(i+1)
+#             ans.pop()
+
+# dfs(1)
+
+# 15651 백트래킹
+n,m = map(int,input().split())
+ans = []
+
+def dfs():
+    if len(ans) == m:
+        print(" ".join(map(str,ans)))
+        return
+        
+    for i in range(1,n+1):
+        ans.append(i)
+        dfs()
+        ans.pop()
+
+dfs()
