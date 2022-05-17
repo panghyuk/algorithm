267a797068fc11c024c1dddcaab1d0e8d8bc90f4
panghyuk <okh9712@gmail.com>
2022-05-17 15:22:26 +0900

    소수 구하기

--- BOJ/수학/배르트랑 공준.py
+++ BOJ/수학/배르트랑 공준.py
@@ -1,32 +1,32 @@
 # 4948
-import sys
+import sys,math
 input = sys.stdin.readline
 
 def prime(x):
     if x == 1:
         return False
-    else:
-        for i in range(2,int(x**(1/2)) + 1):
-            if x % i == 0:
-                return False
-        return True
+    
+    for i in range(2,int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
 
-all_list = [range(2,246913)]
+all_list = list(range(2,246913))
 prime_num = []
 
-for i in all_list:
-    if prime(i):
-        prime_num.append(i)
+for j in all_list:
+    if prime(j):
+        prime_num.append(j)
 
 while True:
-    cnt = 0
     n = int(input())
+    cnt = 0
 
     if n == 0:
         break
-    else:
-        for i in range(n, 2*n + 1):
-            if i in prime_num:
-                cnt += 1
-        
-        print(cnt)
+
+    for i in prime_num:
+        if n < i <= 2 * n:
+            cnt += 1
+    
+    print(cnt)
