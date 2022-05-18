1aa3c6637953f3336ff0f4715b0a19a1ee2c31f4
panghyuk <okh9712@gmail.com>
2022-05-19 01:14:27 +0900

    누적합

--- BOJ/기타 알고리즘/2차원 배열의 합.py
+++ BOJ/기타 알고리즘/2차원 배열의 합.py
@@ -7,6 +7,7 @@ acc = [[0] * (m + 1) for _ in range(n + 1)]
 for _ in range(n):
     graph.append(list(map(int,input().split())))
 
+# 누적합 구하기
 for i in range(1, n + 1):
     for j in range(1, m + 1):
         acc[i][j] = graph[i-1][j-1] + acc[i][j-1] + acc[i-1][j] - acc[i-1][j-1]
@@ -14,6 +15,6 @@ for i in range(1, n + 1):
 k = int(input())
 
 for _ in range(k):
-    i,j,x,y = map(int,input().split())
+    i,j,x,y = map(int,input().split()) # (i,j) -> (x,y)
     ans = acc[x][y] - acc[x][j-1] - acc[i-1][y] + acc[i-1][j-1]
     print(ans)
