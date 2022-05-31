9bd0e08b3ddbeaa6c57c5695a7417d94ebaf8051
panghyuk <okh9712@gmail.com>
2022-06-01 02:33:34 +0900

    복습_DP

--- BOJ/복습/평범한 배낭.py
+++ BOJ/복습/평범한 배낭.py
@@ -1,1 +1,22 @@
 # 12865
+
+n,k = map(int,input().split())
+
+dp = [[0]*(k+1) for _ in range(n+1)]
+weight = []
+value = []
+
+for _ in range(n):
+    w,v = map(int,input().split())
+    weight.append(w)
+    value.append(v)
+    
+for i in range(1,n+1):
+    for j in range(1,k+1):
+    
+        if j < weight[i-1]:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i-1]] + value[i-1])
+
+print(dp[n][k])
