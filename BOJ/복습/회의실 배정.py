ac767d625c5fc358540ed05acb7f595403a13d2d
panghyuk <okh9712@gmail.com>
2022-06-13 01:17:13 +0900

    복습 그리디

---
+++ BOJ/복습/회의실 배정.py
@@ -0,0 +1,22 @@
+# 1931
+
+n = int(input())
+meet = []
+
+for _ in range(n):
+    a,b = map(int,input().split())
+    meet.append([a,b])
+
+meet.sort(key = lambda x: (x[1],x[0]))
+cnt = 0
+end = 0
+
+for i in meet:
+    if end <= i[0]:
+        end = i[1]
+        cnt += 1
+        
+
+print(cnt)
+
+# print(meet)
