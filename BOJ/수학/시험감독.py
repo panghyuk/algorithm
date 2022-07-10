cec30f205d99e6c216252f824cfde9c1d5bcbead
panghyuk <okh9712@gmail.com>
2022-07-10 16:09:50 +0900

    수학

---
+++ BOJ/수학/시험감독.py
@@ -0,0 +1,20 @@
+# 13458
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int,input().split()))
+b,c = map(int,input().split())
+
+cnt = n
+
+for i in a:
+    i -= b
+    if i > 0:
+        if i % c:
+            cnt += (i//c) + 1
+        else:
+            cnt += (i//c)
+
+print(cnt)
