82b7ce908a202ad245fa8410f1b9445545701169
panghyuk <okh9712@gmail.com>
2022-07-25 19:32:52 +0900

    4주차 스터디

--- _스터디 문제풀이/4주차/2206.py
+++ _스터디 문제풀이/4주차/2206.py
@@ -8,15 +8,15 @@ graph = []
 for _ in range(n):
     graph.append(list(map(int,input())))
 
-# print(graph)
-
 cnt = 0
+ans = 0
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 visit = [[0] * m for _ in range(n)]
 
 def bfs(a,b):
+    global ans
     q = deque()
     q.append([a,b])
     while q:
@@ -27,18 +27,22 @@ def bfs(a,b):
             ny = y + dy[k]
 
             if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] ==0 and visit[nx][ny] == 0:
+                if graph[nx][ny] == 0 and visit[nx][ny] == 0:
                     visit[nx][ny] = 1
                     q.append([nx,ny])
-
-ans = 0
+                    ans += 1
+                
 
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 1 and cnt == 0:
-            cnt += 1
-            graph[i][j] = 0
+        if i == 0 and j == 0:
             bfs(i,j)
-        elif graph[i][j] == 0:
-            bfs(i,j)
-            ans += 1
+        else:
+            if graph[i][j] == 1:
+                graph[i][j] = 0
+                cnt = 1
+                bfs(i,j)
+            else:
+                bfs(i,j) 
+    
+print(ans)
