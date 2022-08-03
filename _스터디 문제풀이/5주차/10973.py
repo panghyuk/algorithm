bf8f94801811b3ccda23b6fcc51eb2474ece8dad
panghyuk <okh9712@gmail.com>
2022-08-03 15:20:41 +0900

    5주차 스터디

--- _스터디 문제풀이/5주차/10973.py
+++ _스터디 문제풀이/5주차/10973.py
@@ -1,6 +1,25 @@
 # 이전 순열
 
 n = int(input())
+n_list = list(map(int,input().split()))
+temp = -1
 
-n_list = map(int,input().split())
+for i in range(n):
+    if n_list[i-1] > n_list[i]:
+        temp = i-1
 
+if temp == -1: # 오름차순인 경우
+    print(temp)
+    
+else:
+    for j in range(temp + 1, n):
+        if n_list[j] < n_list[temp]:
+            fix = j
+    
+    # 순서 변경
+    n_list[temp], n_list[fix] = n_list[fix], n_list[temp]
+
+    ans = n_list[:temp + 1] + sorted(n_list[temp + 1:], reverse = True)
+
+    for k in ans:
+        print(k, end = " ")
--- _스터디 문제풀이/5주차/15686.py
+++ _스터디 문제풀이/5주차/15686.py
@@ -1,1 +1,20 @@
 # 치킨 배달
+# 0: 빈칸
+# 1: 집
+# 2: 치킨집
+
+n,m = map(int,input().split())
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int,input().split())))
+
+chickens = []
+homes = []
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1: # 집
+            homes.append([i,j])
+        elif graph[i][j] == 2: # 치킨집
+            chickens.append([i,j])
