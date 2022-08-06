38c70cd0ac07d19bba229a3ef1b7b4fd10532239
panghyuk <okh9712@gmail.com>
2022-08-06 12:58:54 +0900

    6주차 풀이

--- BOJ/구현/한 줄로 서기.py
+++ BOJ/구현/한 줄로 서기.py
@@ -1,2 +1,18 @@
 # 1138
 
+n = int(input())
+h = list(map(int,input().split()))
+ans = [0] * n
+
+for i in range(n):
+    cnt = 0
+    for j in range(n):
+        if cnt == h[i] and ans[j] == 0:
+            ans[j] = i + 1
+            break
+
+        elif ans[j] == 0:
+            cnt += 1
+
+for k in ans:
+    print(k, end = ' ')
--- _스터디 문제풀이/6주차/15486.py
+++ _스터디 문제풀이/6주차/15486.py
@@ -1,1 +1,43 @@
 # 퇴사 2
+
+''' sol.1'''
+n = int(input())
+t = []
+p = []
+dp = [0] * (n + 1)
+
+for _ in range(n):
+    a,b = map(int,input().split())
+    t.append(a)
+    p.append(b)
+
+for i in range(n):
+    if t[i] <= n - i: # 남은 날 안에 끝낼 수 있는지 확인
+        dp[i + t[i]] = max(dp[i + t[i]], dp[i] + p[i])
+        
+    dp[i + 1] = max(dp[i + 1], dp[i])
+    
+print(dp[n])
+
+''' sol.2 '''
+# import sys
+
+# input = sys.stdin.readline
+# n = int(input())
+
+# dp = [0] * (n+1)
+# time = []
+# profit = []
+
+# for _ in range(n):
+#     t,p = list(map(int,input().split()))
+#     time.append(t)
+#     profit.append(p)
+
+# for i in range(n-1,-1,-1):
+#     if time[i] + i > n:
+#         dp[i] = dp[i+1]
+#     else:
+#         dp[i] = max(profit[i] + dp[i + time[i]], dp[i+1])
+
+# print(dp[0])
