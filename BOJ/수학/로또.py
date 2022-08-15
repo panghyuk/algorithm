827a69b8e0376e343a47f40d59b4210a1123de9c
panghyuk <okh9712@gmail.com>
2022-08-15 13:39:37 +0900

    수학

--- BOJ/수학/로또.py
+++ BOJ/수학/로또.py
@@ -1,1 +1,18 @@
 # 6603
+
+from itertools import combinations
+
+while True:
+    n = list(map(int,input().split()))
+
+    if n[0] == 0:
+        exit()
+    else:
+        lotto = n[1:]
+        ans = list(combinations(lotto,6))
+
+        for i in ans:
+            for j in i:
+                print(j, end = " ")
+            print()
+        print()
