b4af04b8d93c69a1a993ac1baa923003bd5852d4
panghyuk <okh9712@gmail.com>
2022-08-15 14:36:36 +0900

    8주차 스터디

--- _스터디 문제풀이/8주차/14501.py
+++ _스터디 문제풀이/8주차/14501.py
@@ -1,2 +1,19 @@
 # 퇴사
 
+n = int(input())
+t = []
+p = []
+dp = [0] * 16
+
+for _ in range(n):
+    time,profit = map(int,input().split())
+    t.append(time)
+    p.append(profit)
+
+for i in range(n-1,-1,-1): # -1씩 거꾸로
+    if t[i] + i > n:
+        dp[i] = dp[i+1]
+    else:
+        dp[i] = max(dp[i+1], p[i] + dp[i + t[i]])
+
+print(dp[0])
--- _스터디 문제풀이/8주차/14888.py
+++ _스터디 문제풀이/8주차/14888.py
@@ -1,2 +1,40 @@
 # 연산자 끼워넣기
 
+from itertools import permutations
+
+n = int(input())
+n_list = list(map(int,input().split()))
+opr_num = list(map(int,input().split()))
+opr_list = ['+', '-', '*', '/']
+opr = []
+
+for i in range(4):
+    for j in range(opr_num[i]):
+        opr.append(opr_list[i])
+
+maxnum = 0
+minnum = 101
+permu = permutations(opr,n-1)
+
+for k in permu:
+    ans = n_list[0]
+    for l in range(1,n):
+        if k[l-1] == '+':
+            ans += n_list[l]
+
+        elif k[l-1] == '-':
+            ans -= n_list[l]
+
+        elif k[l-1] == '*':
+            ans *= n_list[l]
+
+        elif k[l-1] == '/':
+            ans = int(ans / n_list[l])
+    
+    if ans > maxnum:
+        maxnum = ans
+    if ans < minnum:
+        minnum = ans
+
+print(maxnum)
+print(minnum)
