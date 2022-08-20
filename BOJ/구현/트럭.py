39f413f496e02411358f4f309aab299f647f47bd
panghyuk <okh9712@gmail.com>
2022-08-20 22:35:14 +0900

    구현

--- BOJ/구현/트럭.py
+++ BOJ/구현/트럭.py
@@ -12,9 +12,10 @@ while bridge:
     ans += 1
     bridge.pop(0)
 
-    if bridge:
+    if truck: # 남아있는 트럭 확인
         if sum(bridge) + truck[0] <= l: # 다리의 하중보다 작으면 bridge에 추가
             bridge.append(truck.pop(0))
+
         else: # 다리의 하중보다 크면 빈 공간 추가
             bridge.append(0)
 
