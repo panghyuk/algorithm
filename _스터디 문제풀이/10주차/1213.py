a82572414205d0917a7555e4c7272274803c3362
panghyuk <okh9712@gmail.com>
2022-08-30 21:47:19 +0900

    10주차

--- _스터디 문제풀이/10주차/1213.py
+++ _스터디 문제풀이/10주차/1213.py
@@ -1,2 +1,26 @@
 # 팰린드롬 만들기
 
+name = list(input())
+name.sort() # 알파벳 순으로 정렬
+word = {}
+
+for i in name:
+    if i in word:
+        word[i] += 1
+    else:
+        word[i] = 1
+
+cnt = 0
+center = ''
+palindrome = ''
+
+for k,v in word.items():
+    if v % 2 == 1: # 개수가 홀수일 때
+        cnt += 1
+        center += k # 중간에 위치하는 문자열 추가
+    palindrome += k * (v//2) 
+
+if cnt > 1: # 중간에 위치하는 문자 개수가 1보다 클 때 팰린드롬 만족 X
+    print("I'm Sorry Hansoo")
+else:
+    print(palindrome + center + palindrome[::-1]) # 팰린드롬 뒤집어서 추가
--- _스터디 문제풀이/10주차/9205.py
+++ _스터디 문제풀이/10주차/9205.py
@@ -1,2 +1,44 @@
 # 맥주 마시면서 걸어가기
 
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+t = int(input())
+
+def bfs(a,b):
+    q = deque()
+    q.append([a,b])
+
+    while q:
+        x,y = q.popleft()
+        if abs(x - festival[0]) + abs(y - festival[1]) <= 1000: # 갈 수 있는 거리: 맥주 20병 * 50미터
+            return 1
+
+        for i in range(n):
+            if not visit[i]:
+                nx, ny = gs[i]
+
+                if abs(x - nx) + abs(y - ny) <= 1000:
+                    q.append([nx,ny])
+                    visit[i] = 1
+    return 0
+
+for _ in range(t):
+    n = int(input())
+    gs = []
+    home = list(map(int,input().split()))
+    
+    for i in range(n):
+        gs.append(list(map(int,input().split())))
+           
+    festival = list(map(int,input().split()))
+
+    visit = [0 for _ in range(n+1)] # 
+
+    flag = bfs(home[0], home[1])
+    
+    if flag == 1:
+        print('happy')
+    else:
+        print('sad')
